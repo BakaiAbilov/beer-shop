f004afb7b3786084f4103c41b23ffb1a2367aceb
BakaiAbilov <120376707+BakaiAbilov@users.noreply.github.com>
2023-03-28 23:48:25 +0600

    create request for all goods

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React  from "react";
+import { requests } from "./configs";
 
 const App = () => {
+  React.useEffect(() => {
+    requests.getAllGoods('2', '20')
+    .then(res => console.log(res))
+  }, [] )
   return (
     <div className="App">
       app
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,5 +3,9 @@ const initialState = {
 }
 
 export const reducer = (state = initialState, action) => {
-    
+    switch (action.type) {
+
+        default:
+            return state
+    }
 }
