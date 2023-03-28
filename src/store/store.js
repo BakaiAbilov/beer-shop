cb451951e642c2108329e15afdf145a11cde4be7
BakaiAbilov <120376707+BakaiAbilov@users.noreply.github.com>
2023-03-28 23:27:29 +0600

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { 
+    applyMiddleware, 
+    legacy_createStore as createStore 
+} from "redux";
+import { reducer } from "./reducer";
+
+const store = createStore(reducer)
+
+export default store
